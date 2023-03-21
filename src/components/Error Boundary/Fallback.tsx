390372da755c98f793e430f4fc096bb3f888d6f1
aniket <aniket.umbc@gmail.com>
2023-03-21 08:02:05 +0530

    error messge added in center

--- src/components/Error Boundary/Fallback.tsx
+++ src/components/Error Boundary/Fallback.tsx
@@ -3,16 +3,16 @@ import { Error } from '../../utils';
 
 const Fallback: React.FC<Error> = ({ errorCode }) => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div style={{ width: '60%', margin: '0 auto', textAlign: 'center' }}>
       <h3> Something went wrong</h3>
       <h4> Error Code: {errorCode}, Please try again </h4>
-      <h2>
+      <h5>
         The 405 Method Not Allowed error occurs when the web server is
         configured in a way that does not allow you to perform a specific action
         for a particular URL. It’s an HTTP response status code that indicates
         that the request method is known by the server but is not supported by
         the target resource.
-      </h2>
+      </h5>
     </div>
   );
 };
